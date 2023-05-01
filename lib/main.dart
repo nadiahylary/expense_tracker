53abd3316c29a61ac5b20b65115257540665d1cf
Nadia HYLARY <nhylary12@gmail.com>
2023-05-01 19:58:19 +0100

    styled the list of transactions section

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
+import 'package:expense_tracker/transaction.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 void main() {
   runApp(const MyApp());
@@ -15,38 +17,110 @@ class MyApp extends StatelessWidget {
       title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.brown,
+        //colorScheme: ColorScheme.dark(),
       ),
-      home: const MyHomePage(),
+      home: MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatelessWidget {
-  const MyHomePage({super.key});
+  final List<Transaction> transactions = [
+    Transaction(
+        id: "#NT0",
+        title: "Weekly Groceries",
+        amount: 18.25,
+        date: DateTime.now()
+    ),
+    Transaction(
+        id: "#NT1",
+        title: "Hair Styling",
+        amount: 24.99,
+        date: DateTime.now()
+    ),
+    Transaction(
+        id: "#NT2",
+        title: "Monthly transport fair",
+        amount: 31.60,
+        date: DateTime.now()
+    ),
+    Transaction(
+        id: "#NT3",
+        title: "Jim Rohn Books",
+        amount: 58.45,
+        date: DateTime.now()
+    ),
+  ];
+  MyHomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text("Expense Tracker"),
-        backgroundColor: Colors.deepOrangeAccent,
+        backgroundColor: Colors.teal,
       ),
       body: Column(
+        //mainAxisAlignment: MainAxisAlignment.spaceAround,
         children: <Widget>[
           Container(
             width: double.infinity,
-            // height: 250,
             child: const Card(
               elevation: 5,
-              color: Colors.blueGrey,
-              //child: Container(
-              //
-              child: Text("Expenses Chart"),
-              //),
+              color: Colors.deepOrangeAccent,
+              child: Text("Expenses Chart",),
             ),
           ),
-          const Card(
-            child: Text("List of Transactions of Current Day"),
+          Column(
+            children: transactions.map((tx) {
+              return Card(
+                child: Row(
+                  children: <Widget>[
+                    Container(
+                      width: 80,
+                      height: 60,
+                      padding: EdgeInsets.all(10),
+                      margin: EdgeInsets.symmetric(
+                        vertical: 10,
+                        horizontal: 15,
+                      ),
+                      alignment: Alignment.center,
+                      decoration: BoxDecoration(
+                          border: Border.all(color: Colors.deepOrangeAccent,
+                          width: 1,
+                          )),
+                      child: Text(
+                        "\$${tx.amount.toString()}", //'\$'+ tx.amount.toString()
+                        style: TextStyle(
+                          fontWeight: FontWeight.w600,
+                          fontSize: 18,
+                          color: Colors.deepOrangeAccent,
+                        ),
+                      ),
+                    ),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                      Text(tx.title,
+                          style: TextStyle(
+                            fontWeight: FontWeight.w500,
+                            fontSize: 18,
+                            color: Colors.teal,
+                          )
+                      ),
+                      Text(DateFormat.yMMMd().format(tx.date),
+                          style: TextStyle(
+                            fontWeight: FontWeight.w300,
+                            fontSize: 13,
+                            color: Colors.brown,
+                          )
+                      ),
+                      ],
+                    )
+                  ],
+                ),
+              );
+            }).toList()
           )
         ],
       ),
